2b833af96149091253cc735398c8079e98b33033
SongoMen <songomengaming@gmail.com>
2019-12-10 16:38:31 +0100

    s

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .api import TestViewSet
 
 router = routers.DefaultRouter()
-router.register('api/test', TestViewSet, 'test')
+router.register('api', TestViewSet, 'test')
 
 urlpatterns = router.urls
